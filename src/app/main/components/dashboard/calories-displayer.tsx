ebc42f5bea90cab9257e571640b50d42bf6c2581
RobuGabrie <gabriel.robu@student.usv.ro>
2024-04-17 16:20:51 +0300

    Created initial scratch for main page

---
+++ src/app/main/components/dashboard/calories-displayer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const percentage = 66;
+
+function CaloriesCounter() {
+  return (
+    <div className="grid-span-1 border rounded-lg bg-popover shadow-md w-full h-full">
+    
+    </div>
+  );
+}
+
+export default CaloriesCounter;
